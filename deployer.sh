3af31c240a2e681159e3c6bc9e13ad1afd6f1b03
Vibhas Goyal <vibhas777@gmail.com>
2019-05-18 03:13:18 +0530

    ./deployer.sh

--- deployer.sh
+++ deployer.sh
@@ -4,18 +4,16 @@ hugo
 
 cd themes/hyde
 git add .
-git commit -m "update"
+git commit -m $0
 git push
 cd ../..
 
 cd public
 git add .
-git commit -m "update"
+git commit -m $0
 git push
 cd ..
 
 git add .
-git commit -m "update"
+git commit -m $0
 git push
-
-
